fdea1e4fbb259d64d2f9595b7f3217e75f9d9979
mohetize <mohetize@gmail.com>
2025-05-13 17:47:46 +0100

    this change for index and script and style

---
+++ script.js
@@ -0,0 +1,7 @@
+//script for the main page
+console.log("Hello, World!");
+// Function to handle the form submission
+function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted");
+}
